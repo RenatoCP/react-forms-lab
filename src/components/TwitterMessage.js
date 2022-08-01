978169875cf55ec839010271ef6407c7ea8e5920
RenatoCP <renato_pinha@icloud.com>
2022-08-02 08:36:42 +1000

    done

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,24 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message:""
+    };
+  }
+
+  handleMessageInput = (event) => {
+    this.setState({
+      message: event.target.value
+    })
   }
 
   render() {
+    let charsRemain = this.props.maxChars - this.state.message.length
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" onChange={(event) => this.handleMessageInput(event)} value={(this.state.messsage)}/>
+        {charsRemain}
       </div>
     );
   }
